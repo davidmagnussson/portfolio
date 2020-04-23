698d42bb5ebfea08ee4f67bf1d08ba31256b8e43
davidmagnussson <davidmagnusson97@gmail.com>
2020-04-24 01:20:11 +0200

    Updates

---
+++ precache-manifest.88805510b239d0da7c20a643abc7cd98.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9d65e9aa65114025395be5939e7159",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bd87540b8bad69b616ad",
+    "url": "/portfolio/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "db0b113cc9c97d95e29c",
+    "url": "/portfolio/static/css/main.7b7eec67.chunk.css"
+  },
+  {
+    "revision": "bd87540b8bad69b616ad",
+    "url": "/portfolio/static/js/2.567e3065.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.567e3065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0b113cc9c97d95e29c",
+    "url": "/portfolio/static/js/main.4a6abb98.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "07ae37d7b7d7054d3fca41213146e955",
+    "url": "/portfolio/static/media/StudyGuide.07ae37d7.PNG"
+  },
+  {
+    "revision": "1fdab28e93e527b5c91963030c141115",
+    "url": "/portfolio/static/media/Visualizing_groups.1fdab28e.PNG"
+  },
+  {
+    "revision": "f19351f07ac65cbfd8a8d14102474d52",
+    "url": "/portfolio/static/media/clintri.f19351f0.PNG"
+  },
+  {
+    "revision": "84d489183df4578815a4443152b494b8",
+    "url": "/portfolio/static/media/default.84d48918.PNG"
+  },
+  {
+    "revision": "07402ffcd9c5ac22acc82ee037de32b6",
+    "url": "/portfolio/static/media/firebase.07402ffc.png"
+  },
+  {
+    "revision": "d058f862bdc861e147fd6e3196b89d4b",
+    "url": "/portfolio/static/media/html.d058f862.jpg"
+  },
+  {
+    "revision": "23e9beb6b3f9005402a5ca0d7524375e",
+    "url": "/portfolio/static/media/js.23e9beb6.png"
+  },
+  {
+    "revision": "8f5130c84d4f580f6387d7e5d4d7976d",
+    "url": "/portfolio/static/media/meteor.8f5130c8.png"
+  },
+  {
+    "revision": "bfbab346d25d6617159c28a4189727b1",
+    "url": "/portfolio/static/media/profilbild.bfbab346.jpg"
+  },
+  {
+    "revision": "6a324081941cbd6a3099e998c1cc2530",
+    "url": "/portfolio/static/media/ps.6a324081.jpg"
+  },
+  {
+    "revision": "57b508214d06e292b5e6fb5df2f4a089",
+    "url": "/portfolio/static/media/react.57b50821.jpg"
+  },
+  {
+    "revision": "1665b736394ed15dd30a9780d85cf8e4",
+    "url": "/portfolio/static/media/xd.1665b736.png"
+  }
+]);
